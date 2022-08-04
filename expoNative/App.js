9c1c4fa1ffa7889ccd3f9f11bc0da3764c1ddf6e
alexandre <alexandre@localhost.localdomain>
2022-08-04 14:41:04 +0200

    set up projet native

---
+++ expoNative/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LandingPage from './screens/LandingPage';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LandingPage">
+        <Stack.Screen name="LandingPage" component={LandingPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+/**/
