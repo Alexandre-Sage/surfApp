bfcf35c453c2234f326b1b883d1888ecf7dbcc9a
Alexandre Sage <alexandre.sage@adsofware.fr>
2022-10-17 21:04:34 +0200

    feat: user api ready for dev server

--- backEnd/sharedModules/testModules/assertionModule.ts
+++ backEnd/sharedModules/testModules/assertionModule.ts
@@ -1,4 +1,4 @@
-import chai, { request, assert, should, expect, } from "chai";
+import { expect } from "chai";
 
 export const objectPropertyAssertion = (data: any, propertyArray: string[]) => (
   propertyArray.forEach((property) => expect(data).to.have.property(property))
--- backEnd/userApi/test/sucess/profilHeader.ts
+++ backEnd/userApi/test/sucess/profilHeader.ts
@@ -1,52 +1,14 @@
 import server from "../../server.js";
-import chai, { request, assert, should, expect, } from "chai";
+import chai, { expect } from "chai";
 import chaiHttp from "chai-http"
 import { Suite } from "mocha";
-<<<<<<< HEAD
-import { Response } from "express"
-import { ServerResponse } from "http";
 import { getAuthentificationToken } from "../../../sharedModules/testModules/login.js"
-import fetch from "node-fetch";
 import registry from "../../../../urlRegistry.mjs"
 const { devloppmentServer } = registry;
 
 chai.use(chaiHttp)
-const data = [{
-   location: 'TestOne',
-   name: 'TestOne',
-   firstName: 'TestOne',
-   userName: 'TestOne',
-   creationDate: "2022-10-16T10:38:17.000Z"
-}]
+
 const url = `${devloppmentServer.authApi}/logIn`
-export default function getUserHeaderSucessTest(): Suite {
-   return describe.only("LOG IN AND GET USER HEADER INFO", function () {
-      it("Should log in and get user header json info", async () => {
-         const agent = chai.request.agent(server);
-         const credentials = { email: "test@testOne.com", password: "test" };
-         const responseMessage = data;
-         const contentType = 'application/json; charset=utf-8';
-         const contentLength = '126';
-         try {
-            const token: any = await getAuthentificationToken(url, credentials)
-            const response = await agent.get("/header").set("Authorization", `Bearer ${token.token}`);
-            const { header, body, error } = response;
-            expect(error).to.be.eql(false);
-            expect(response).to.have.property("status").eql(200);
-            expect(body).to.be.eql(responseMessage);
-            expect(header).to.have.property('content-type').eql(contentType);
-            expect(header).to.have.property('content-length').eql(contentLength);
-            expect(header).to.have.property('access-control-allow-credentials').eql("true");
-         } catch (error: any) {
-            throw error
-         };
-      });
-   });
-=======
-import { getAuthentificationToken } from "../../../sharedModules/testModules/login.js"
-
-chai.use(chaiHttp)
-
 const data = [{
   location: 'TestOne',
   name: 'TestOne',
@@ -54,8 +16,6 @@ const data = [{
   userName: 'TestOne',
   creationDate: "2022-10-16T10:38:17.000Z"
 }]
-const url = "http://192.168.1.39:3500/logIn"
-
 export default function getUserHeaderSucessTest(): Suite {
   return describe("LOG IN AND GET USER HEADER INFO", function () {
     it("Should log in and get user header json info", async () => {
@@ -79,7 +39,6 @@ export default function getUserHeaderSucessTest(): Suite {
       };
     });
   });
->>>>>>> feat/migration_user_api
 };
 
 
