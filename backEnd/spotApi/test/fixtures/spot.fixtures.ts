029056c0584e096845c6a06dda4c2220bcd761cd
Alexandre Sage <alexandre.sage@adsoftware.fr>
2022-12-18 11:37:43 +0100

    chore --stages

--- backEnd/spotApi/test/fixtures/spot.fixtures.ts
+++ backEnd/spotApi/test/fixtures/spot.fixtures.ts
@@ -1,26 +1,29 @@
 import { Types } from "mongoose";
+import { SpotInterface } from "../../../mongoDb/spots/spotInterface";
 
-const spotFactory = (userId: Types.ObjectId) => ({
-  spotName: "port blanc",
-  country: "France",
-  userId,
-  type: {
-    waveType: "Shore break",
-    bottomType: "Sand",
-  },
-  location: {
-    type: "Point",
-    coordinates: ["47.52408959", "-3.1545563"]
-  },
-  orientation: ["W", "N/W", "WN/W"],
-  optimalConditions: {
-    wind: {
-
+const spotFactory = (spot: Partial<SpotInterface>) => {
+  return ({
+    spotName: "port blanc",
+    country: "France",
+    userId: spot.userId,
+    type: {
+      waveType: "Shore break",
+      bottomType: "Sand",
     },
-    swell: {
+    location: {
+      type: "Point",
+      coordinates: ["47.52408959", "-3.1545563"]
+    },
+    orientation: ["W", "N/W", "WN/W"],
+    optimalConditions: {
+      wind: {
 
-    }
-  },
-  creationDate: new Date().toUTCString()
-});
+      },
+      swell: {
+
+      }
+    },
+    creationDate: new Date().toUTCString()
+  })
+};
 export { spotFactory }
